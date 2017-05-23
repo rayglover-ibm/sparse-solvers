96fe3bd52d801866fc9f7e5b6bd21661e966dd5a
Ray Glover <ray.glover@uk.ibm.com>
2017-05-23 15:56:11 +0100

    [python] simple unit test. #2

--- bindings/python/sparsesolvers/binding_test.py
+++ bindings/python/sparsesolvers/binding_test.py
@@ -1,3 +1,24 @@
+import unittest
 import sparsesolvers as ss
+import numpy as np
 
-print("[py] version={}".format(ss.version()))
+class HomotopySolverTest(unittest.TestCase):
+    def test_smoke(self):
+        solver = ss.Homotopy()
+
+        N = 5
+        A = np.identity(N)
+
+        for n in range(0, N-1):
+            signal = np.zeros(N)
+            signal[n] = 1
+
+            x, info = solver.solve(A, signal)
+            np.array_equal(signal, x)
+
+            assert(info.solution_error == 0)
+            assert(info.iter == 1)
+
+if __name__ == '__main__':
+    print("[sparsesolvers] version={}".format(ss.version()))
+    unittest.main()
