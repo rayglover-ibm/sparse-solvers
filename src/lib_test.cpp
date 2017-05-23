48af4cbdb9ae4f39659dab7d0d970fb98ecbefbf
Ray Glover <ray.glover@uk.ibm.com>
2017-05-23 15:49:19 +0100

    move c++ tests in to src

--- src/lib_test.cpp
+++ src/lib_test.cpp
@@ -16,4 +16,21 @@ TEST(ndspan, 2d_constructors)
     EXPECT_ANY_THROW(( ss::ndspan<float, 2>(data, { 4, 1 }) ));
     /* n > 0 */
     EXPECT_ANY_THROW(( ss::ndspan<float, 2>(data, { 8, 0 }) ));
+
+    EXPECT_EQ(&data.at(0), mat.data());
+    EXPECT_EQ(&data.at(0), mat2.data());
+}
+
+TEST(ndspan, 1d_constructors)
+{
+    std::array<float, 8> data{ 1, 2, 3, 4, 5, 6, 7, 8 };
+    ss::ndspan<float> spanA(data);
+
+    EXPECT_EQ(data.size(), spanA.shape[0]);
+    EXPECT_EQ(data[1], spanA.span[1]);
+
+    ss::ndspan<float> spanB(&data[4], { 1 });
+
+    EXPECT_EQ(1, spanB.shape[0]);
+    EXPECT_EQ(spanB.span[0], 5);
 }
