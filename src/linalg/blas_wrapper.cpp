a7ad5b3e50e6c8c62e56465755adc4d43744f4c0
ray glover <ray.glover@uk.ibm.com>
2017-09-30 23:55:31 +0100

    blas runtime library selection and loading

--- src/linalg/blas_wrapper.cpp
+++ src/linalg/blas_wrapper.cpp
@@ -14,24 +14,56 @@ limitations under the License.  */
 #include "linalg/blas_wrapper.h"
 #include "linalg/blas_prelude.h"
 
+#include "kernelpp/kernel.h"
+#include "kernelpp/kernel_invoke.h"
+
 namespace ss {
 namespace blas
 {
+    /* cblas --------------------------------------------------------------- */
+
+    using namespace kernelpp;
+
+    /* Kernel to perform feature detection for the runtime cpu/gpu */
+    struct cblas::loader : kernelpp::impl<loader, compute_mode::AVX, compute_mode::CPU>
+    {
+        static constexpr const char* name = "cblas::loader";
+        template<compute_mode> static error_code op();
+    };
+
     cblas* cblas::get()
     {
         if (!m) { configure(); }
         return m.get();
     }
 
-    void cblas::configure() {
-        m.reset(new cblas(BLAS_RUNTIME_FILE));
-        /* if an error occured, abort */
-        if (m->error()) {
-            fprintf(stderr, "%s\n", m->error().value().c_str());
+    void cblas::configure()
+    {
+        /* if an error occured when loading blas, abort */
+        if (kernelpp::run<loader>()) {
+            const char* msg = m && m->error() ?
+                m->error().value().c_str() : "Failed to load cblas";
+
+            fprintf(stderr, "%s\n", msg);
             abort();
         }
     }
 
+    /* static cblas instance */
     std::unique_ptr<cblas> cblas::m;
+
+    /* avx cpu blas */
+    template <> error_code cblas::loader::op<compute_mode::AVX>()
+    {
+        cblas::m.reset(new cblas(BLAS_AVX_RUNTIME_FILE));
+        return cblas::m->error() ? error_code::KERNEL_FAILED : error_code::NONE;
+    }
+
+    /* standard cpu blas */
+    template <> error_code cblas::loader::op<compute_mode::CPU>()
+    {
+        cblas::m.reset(new cblas(BLAS_RUNTIME_FILE));
+        return cblas::m->error() ? error_code::KERNEL_FAILED : error_code::NONE;
+    }
 }
 }
