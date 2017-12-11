0859b51fb57e08e5710d397c6df8007bb02ff730
ray glover <ray.glover@uk.ibm.com>
2017-12-11 22:06:52 +0000

    update python reference irls implementation

--- bindings/python/reference/irls.py
+++ bindings/python/reference/irls.py
@@ -1,69 +1,22 @@
 #iterative reweighted least squares algorithm for sparse decomposition
 
 import numpy as np
-import math
-import common as helpers
+import numpy.linalg as lin
+import scipy.linalg as scilin
 
-def update_x(A, y, w):
-    """
-    Function:  update_x
-    --------------------
-    updates x from previous iteration to the next
+np.set_printoptions(precision=6, suppress=True)
 
-        A: sensing matrix
-        y: signal vector
-        w: weights
+def irls_newton(Q, R, b, w):
+    C, upper = scilin.cho_factor(np.dot(Q.T, Q * w))
+    s = scilin.cho_solve((C, upper), np.dot(Q.T, b))
+    t = np.dot(Q, s)
 
-    returns: new updated approximation to the solution x
-    """
+    x = lin.solve(R, np.dot(Q.T, t))
+    x /= lin.norm(x, ord=1)
 
-    # find number of weights
-    N = len(w)
+    return x
 
-    # construct diagonal matrix
-    Dmat = np.zeros((N, N))
-    for i in range(0,N):
-        Dmat[i, i] = w[i]
-
-    # find transpose of A
-    A_t = np.matrix.transpose(A)
-
-    # invert diagonal matrix
-    Dmat_inv = np.linalg.inv(Dmat)
-
-    # multiply inverse of D with transpose of A
-    Dinv_At = np.dot(Dmat_inv, A_t)
-
-    # invert ADA matrix
-    ADA_inv = np.linalg.inv(np.dot(A, Dinv_At))
-
-    # evaluate the transformation matrix
-    pref_mat = np.dot(Dinv_At, ADA_inv)
-
-    # evaluate x by applying the transformation matrix to y
-    return np.dot(pref_mat, y)
-
-
-def update_w(epsi, x):
-    """
-    Function:  update_w
-    --------------------
-    updates weights from previous iteration to the next
-
-        epsi: current value of epsilon
-        x: current best approximation to the solution x
-
-    returns: new updated set of weights
-    """
-
-    N = len(x)
-    w = np.zeros(N)
-    for i in range(0, N):
-        w[i] = 1.0 / math.sqrt((x[i] * x[i]) + (epsi * epsi))
-    return w
-
-
-def irls(A, b, N_iter, K, tol):
+def irls(A, b, n_iter, K, tol):
     """
     Function:  irls
     --------------------
@@ -72,43 +25,50 @@ def irls(A, b, N_iter, K, tol):
 
         A: sensing matrix
         y: signal
-        N_iter: maximum number of iterations
+        n_iter: maximum number of iterations
         K: sparsity budget
 
     returns: the sparse representation vector x
     """
-
     (M, N) = A.shape
+    Q, R = lin.qr(A, mode='complete')
 
     # set up system
-    epsi = 1.0          # initialise epsilon
-    w = np.ones(N)      # initialise weights vector
-    x_sol = np.zeros(N) # initialise trial solution
+    epsi = 1.0      # initialise epsilon
+    w = np.ones(N)  # initialise weights vector
 
     # run iterative procedure
-    for iIter in range(0, N_iter):
+    for _ in range(0, n_iter):
         # update x
-        x_sol = update_x(A, b, w)
+        x = irls_newton(Q, R, b, w)
+        print ("x={}".format(x))
 
         # sort r to get r-vector
-        r_vec = np.sort(helpers.elementwise_absolute(x_sol), kind='mergesort')  # sort in ascending order
-        # flip into descending order
-        r_vec = r_vec[::-1]
+        xsorted = np.sort(np.abs(x), kind='mergesort')[::-1] # sort in descending order
 
         # check if sparsity achieved yet
-        if math.fabs(r_vec[K+1] < tol):
+        if np.abs(xsorted[K + 1]) < tol:
             break
 
         # update epsilon if required
-        testVal = r_vec[K + 1] / float(N)
-        if testVal < epsi:
-            epsi = testVal
+        epsi = min(epsi, xsorted[K + 1] / float(N))
+        print ("epsi={}".format(epsi))
 
         # update weights
-        w = update_w(epsi, x_sol)
-
-    # note number of iterations used
-    print('sparse solver finished after ' + str(iIter + 1) + ' out of max ' + str(N_iter) + ' iterations')
+        w = 1.0 / np.sqrt(x * x + epsi * epsi)
+        print ("w={}\n".format(w))
 
     # return final solution
-    return x_sol
+    return x
+
+
+b = np.asarray([.0, .5, .45, .05])
+A = np.array([
+    [.1, .2, .0, .2],
+    [.4, .4, .8, .4],
+    [.5, .3, .1, .4],
+    [.0, .1, .1, .0]
+])
+
+x = irls(A, b, 20, 1, 0.01)
+print("Ax=\n{0}\n".format(A.dot(x)))
